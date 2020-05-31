e30f757437fb7e090ffb0b63b3dcdaec2be54bf5
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-05-31 12:28:07 +0200

    Add main menu buttons

--- src/swe4/view/MainView.java
+++ src/swe4/view/MainView.java
@@ -1,25 +1,52 @@
 package swe4.view;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import swe4.model.DataModel;
 
 public class MainView {
 
   public static Scene create() {
-      TabPane mainMenu = new TabPane();
-      Tab tabOrders = new Tab("Bestellungen");
-      Tab tabMenu = new Tab("Speisekarte");
-      Tab tabTimeSlots = new Tab("Zeitbereiche");
-      Tab tabUsers = new Tab("Benutzerverwaltung");
+      BorderPane mainLayout = new BorderPane();
+      mainLayout.setTop(mainMenuButtons());
+      mainLayout.setCenter(mainMenuTabs());
+      return new Scene(mainLayout, 800, 400);
+  }
+
+  private static HBox mainMenuButtons() {
+      HBox mainMenuButtons = new HBox();
+      Label lastSavedLabel = new Label("Zuletzt gespeichert um 11:34 Uhr");
+      Button saveButton = new Button("Speichern");
+      Button openButton = new Button("Öffnen");
+      Button logoutButton = new Button("Ausloggen");
+      mainMenuButtons.getChildren().addAll(lastSavedLabel, saveButton, openButton, logoutButton);
+      return mainMenuButtons;
+  }
+
+  private static TabPane mainMenuTabs() {
       DataModel data = new DataModel();
+
+      TabPane mainMenuTabs = new TabPane();
+
+      Tab tabOrders = new Tab("Bestellungen");
       tabOrders.setContent(OrderTab.create(data.getOrders()));
+
+      Tab tabMenu = new Tab("Speisekarte");
       tabMenu.setContent(MenuTab.create(data.getDishes()));
+
+      Tab tabTimeSlots = new Tab("Zeitbereiche");
       tabTimeSlots.setContent(TimeSlotsTab.construct(data.getTimeSlots()));
+
+      Tab tabUsers = new Tab("Benutzerverwaltung");
       tabUsers.setContent(UsersTab.create(data.getUsers()));
-      mainMenu.getTabs().addAll(tabOrders, tabMenu, tabTimeSlots, tabUsers);
-      mainMenu.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
-      return new Scene(mainMenu, 800, 400);
+
+      mainMenuTabs.getTabs().addAll(tabOrders, tabMenu, tabTimeSlots, tabUsers);
+      mainMenuTabs.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+      return mainMenuTabs;
   }
 }
