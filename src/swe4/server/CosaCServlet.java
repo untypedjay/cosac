4fdd729ed37ef911a0dbba5c6e3420608f8dde5b
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-06-21 23:41:38 +0200

    Add CosaCServlet

---
+++ src/swe4/server/CosaCServlet.java
@@ -0,0 +1,24 @@
+package swe4.server;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CosaCServlet extends HttpServlet {
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+  }
+
+  @Override
+  public void destroy() {
+
+  }
+
+  @Override
+  public void init() throws ServletException {
+    super.init();
+  }
+}
