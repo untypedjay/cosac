8c40d741a59491b26cca38d834522bf474fa65bb
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-06-22 23:47:36 +0200

    Add basic structure of doGet()

--- src/swe4/server/CosaCServlet.java
+++ src/swe4/server/CosaCServlet.java
@@ -5,11 +5,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class CosaCServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    response.setContentType("text/html");
+    PrintWriter out = null;
+    out = response.getWriter();
+    out.println("<html>");
 
+    out.println("</html>");
   }
 
   @Override
